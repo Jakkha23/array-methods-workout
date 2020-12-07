8532c62d6874506b9200bd88a8a9b2d2ab45fe46
Jakkha23 <jakob.khaytan@skola.botkyrka.se>
2020-12-07 15:58:48 +0100

    added '3_includes.js'

---
+++ 3_includes.js
@@ -0,0 +1,4 @@
+const arr = ['jakop', 'jakob', 'akop', 'aak', 'spak']
+
+console.log(arr.includes('jakob')) // returnernar true eftersom arrayen innehåller 'jakob'
+console.log(arr.includes('katt'))//returnerar false eftersom arrayen inte innehåller 'katt'
